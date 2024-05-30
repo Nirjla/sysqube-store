fd7be8d0cb862b03db8aad98f09336d398cee902
Nirjla <shakyanirjala6@gmail.com>
2024-05-30 20:56:14 +0545

    completed progress bar

--- src/components/Headline.jsx
+++ src/components/Headline.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 const Headline = ({ title, highlight }) => {
   return (
     <div className="  lg:text-[25px] md:text-[24px] sm:text-[22px]  font-semibold text-secondary capitalize leading-[1] py-5 ">
