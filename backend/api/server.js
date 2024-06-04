a19d1ff561f769eb290f385fe7716683252d3f56
Nirjla <shakyanirjala6@gmail.com>
2024-06-04 22:59:51 +0545

    vercel error

--- backend/api/server.js
+++ backend/api/server.js
@@ -5,6 +5,9 @@ const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+}))
 server.use(router)
 
 const port = process.env.PORT || 3030
@@ -12,3 +15,5 @@ const port = process.env.PORT || 3030
 server.listen(port, () => {
     console.log(`JSON Server is running on port ${port}`)
 })
+
+module.exports = server
