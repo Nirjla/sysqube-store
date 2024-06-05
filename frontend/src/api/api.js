66f345155d6a521857e2ba3fbba07c0ae702a249
Nirjla <shakyanirjala6@gmail.com>
2024-06-05 20:12:55 +0545

    README.md added

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.REACT_APP_API_URL,
+    baseUrl: process.env.REACT_APP_API_URL ,
   }),
   endpoints: (builder) => ({
     getProducts: builder.query({
